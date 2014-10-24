0a2f1d4563f39457a1b7c8ee941c2639f14b867f
pam- <pam.assogba@gmail.com>
2014-10-24 15:00:18 -0400

    working on database

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,12 +4,16 @@ class StudentsController < ApplicationController
     @student = Student.all
   end
 
+  def new
+    @student = Student.new
+  end
+
   def create
     @student = Student.new(params[:student])
     if @student.save 
       respond_to do |format|
-        format.html { redirect_to @student}
-        format.json { render :json, @student}
+        format.html { redirect_to @student }
+        format.json { render :json, @student }
       end
     else 
       respond_to do |format|
@@ -21,21 +25,42 @@ class StudentsController < ApplicationController
 
   def show
     @student = Student.find(params[:student])
+
+    respond_to do  |format|
+      format.html
+      format.json { render json: @student }
+    end 
   end
 
   def update
     @student = Student.find(params[:id])
     if @student.update_attributes(params[:student]) 
       respond_to do |format|
-        format.html { redirect_to @student}
-        format.json { render :json, @student}
+        format.html { redirect_to @student }
+        format.json { render json: @student }
       end
     else 
       respond_to do |format|
         format.html { render "edit" }
-        format.json { render status: 404}
+        format.json { render status: 404 }
       end
     end
+  end
+
+  def destroy
+    @student = Student.find(params[:id])
+
+    if @student.destroy
+      respond_to do |format|
+        format.html { redirect_to @students }
+        format.json { render json: {} }
+      end 
+    else
+      respond_to do |format|
+        format.html { redirect_to @student }
+        format.json { render status: 404 }
+      end  
+    end 
   end
 
 end
